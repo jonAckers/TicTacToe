975304ce2d1c653bbf39350d61150f34eaeb7fa6
Jonathon <ja706@cam.ac.uk>
2021-04-01 23:23:54 +0100

    Setup project structure

--- babel.config.js
+++ babel.config.js
@@ -2,5 +2,20 @@ module.exports = function (api) {
 	api.cache(true);
 	return {
 		presets: ['babel-preset-expo'],
+		plugins: [
+			[
+				'module-resolver',
+				{
+					alias: {
+						'@assets': './assets',
+						'@components': './src/components',
+						'@config': './src/config',
+						'@contexts': './src/contexts',
+						'@screens': './src/screens',
+						'@utils': './src/utils',
+					},
+				},
+			],
+		],
 	};
 };
